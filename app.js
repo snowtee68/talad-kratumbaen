e325e7d9a1a7e689ad925013242722341fa3ff19
snowtee68 <snowtee68@gmail.com>
2026-08-23 17:42:38 +0700

    5.20.15

--- app.js
+++ app.js
@@ -1649,7 +1649,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js?v=5.7.9.27', {scope:'./',updateViaCache:'none'}).catch((err) => {
+      navigator.serviceWorker.register('./sw.js?v=5.7.9.30', {scope:'./',updateViaCache:'none'}).catch((err) => {
         console.warn('Service worker registration failed:', err);
       });
     });
