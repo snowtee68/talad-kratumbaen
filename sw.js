355b32c1ec6f56a99c5339275c3afade1589784a
snowtee68 <snowtee68@gmail.com>
2026-08-30 08:55:13 +0700

    5.22.55

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'talad-kratumbaen-v0.5.22.54';
+const CACHE_NAME = 'talad-kratumbaen-v0.5.22.55';
 const CORE = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.webmanifest',
-  './icons/icon-192.png?v=0.5.22.54',
-  './icons/icon-512.png?v=0.5.22.54',
-  './icons/icon-maskable-512.png?v=0.5.22.54',
-  './icons/apple-touch-icon.png?v=0.5.22.54'
+  './icons/icon-192.png?v=0.5.22.55',
+  './icons/icon-512.png?v=0.5.22.55',
+  './icons/icon-maskable-512.png?v=0.5.22.55',
+  './icons/apple-touch-icon.png?v=0.5.22.55'
 ];
 
 self.addEventListener('install', (event) => {
